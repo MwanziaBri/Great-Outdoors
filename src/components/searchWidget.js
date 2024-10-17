ff9afd4f4d2b3c89208cccb52b4c4400d0d34d45
MwanziaBri <brianmmwanzia@gmail.com>
2024-10-17 13:05:30 +0300

    Update searchWidget.js
    Attempt with script solution

--- src/components/searchWidget.js
+++ src/components/searchWidget.js
@@ -1,8 +1,17 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import Script from 'next/script'
 import Head from 'next/head'
 import { Box } from '@chakra-ui/react'
 
+const scripts = [
+  "https://reservations.reserveport.com/checkavailability/static/js/manifest.js",
+  "https://reservations.reserveport.com/checkavailability/static/js/vendor.js",
+  "https://reservations.reserveport.com/checkavailability/static/js/app.js",
+  
+  "https://www.reserveport.com/media/api5/jquery.min.js",
+  "https://www.reserveport.com/media/api5/popper.min.js",
+  "https://www.reserveport.com/media/api5/bootstrap.min.js"
+];
 
 const stylesheets = [
   "https://reservations.reserveport.com/checkavailability/static/css/app.css",
@@ -12,71 +21,62 @@ const stylesheets = [
 
 export default function Home() {
   const [scriptsLoaded, setScriptsLoaded] = useState(false);
+
   useEffect(() => {
+    // Function to dynamically load external scripts
+    const loadScripts = () => {
+      let loadedCount = 0;
+      scripts.forEach(src => {
+        if (!document.querySelector(`script[src="${src}"]`)) {
+          const script = document.createElement("script");
+          script.src = src;
+          script.async = true;
+          script.onload = () => {
+            loadedCount += 1;
+            if (loadedCount === scripts.length) {
+              setScriptsLoaded(true); // All scripts are loaded
+            }
+          };
+          document.body.appendChild(script);
+        }
+      });
+    };
+
     // Function to dynamically load external stylesheets
     const loadStylesheets = () => {
       stylesheets.forEach(href => {
         if (!document.querySelector(`link[href="${href}"]`)) {
           const link = document.createElement("link");
           link.href = href;
-          link.rel = "stylesheet";  // Correctly set the rel attribute to "stylesheet"
-          document.head.appendChild(link); // Append to head, not body
+          link.rel = "stylesheet";
+          document.head.appendChild(link);
         }
       });
     };
 
-    // Load custom script and styles only if not already present
-    loadStylesheets();   // Load the external styles when component mounts
-  }, []);  // Empty dependency array ensures this runs once on component mount
+    loadScripts();
+    loadStylesheets();
+  }, []);
 
-    const handleScriptLoad = () => {
-      // Set scriptsLoaded to true once all necessary scripts are loaded
-      setScriptsLoaded(true);
-    };
-  
-    return (
-      <>
-        <Head>
-          {/* External scripts loaded with Next.js Script component, strategy set to 'beforeInteractive' */}
-          <Script 
-            src="https://reservations.reserveport.com/checkavailability/static/js/manifest.js" 
-            strategy="beforeInteractive" 
-            onLoad={handleScriptLoad} 
-          />
-          <Script 
-            src="https://reservations.reserveport.com/checkavailability/static/js/vendor.js" 
-            strategy="beforeInteractive" 
-            onLoad={handleScriptLoad} 
-          />
-          <Script 
-            src="https://reservations.reserveport.com/checkavailability/static/js/app.js" 
-            strategy="beforeInteractive" 
-            onLoad={handleScriptLoad} 
-          />
-  
-          <Script 
-            src="https://www.reserveport.com/media/api5/jquery.min.js" 
-            strategy="beforeInteractive" 
-            onLoad={handleScriptLoad} 
-          />
-          <Script 
-            src="https://www.reserveport.com/media/api5/popper.min.js" 
-            strategy="beforeInteractive" 
-            onLoad={handleScriptLoad} 
-          />
-          <Script 
-            src="https://www.reserveport.com/media/api5/bootstrap.min.js" 
-            strategy="beforeInteractive" 
-            onLoad={handleScriptLoad} 
-          />
-        </Head>
-  
-        {/* Conditionally render the widget once the scripts are loaded */}
-        {scriptsLoaded ? (
-          <search-availability id="1687" clientemail="true"></search-availability>
-        ) : (
-          <Box>Loading widget...</Box>
-        )}
-      </>
-    );
-  }
+  return (
+    <>
+      <Head>
+        {/* Ensure the external scripts are pre-loaded in the head */}
+        <Script src="https://reservations.reserveport.com/checkavailability/static/js/manifest.js" strategy="beforeInteractive" />
+        <Script src="https://reservations.reserveport.com/checkavailability/static/js/vendor.js" strategy="beforeInteractive" />
+        <Script src="https://reservations.reserveport.com/checkavailability/static/js/app.js" strategy="beforeInteractive" />
+
+        <Script src="https://www.reserveport.com/media/api5/jquery.min.js" strategy="beforeInteractive" />
+        <Script src="https://www.reserveport.com/media/api5/popper.min.js" strategy="beforeInteractive" />
+        <Script src="https://www.reserveport.com/media/api5/bootstrap.min.js" strategy="beforeInteractive" />
+      </Head>
+
+      {/* Render the <search-availability> component only when scripts are loaded */}
+      {scriptsLoaded ? (
+        <search-availability id="1687" clientemail="true"></search-availability>
+      ) : (
+        <Box>Loading widget...</Box>
+      )}
+    </>
+  );
+}
