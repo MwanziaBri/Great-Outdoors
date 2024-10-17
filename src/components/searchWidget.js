737c3937042168f674629a63c8eee9c81f29dd9c
MwanziaBri <brianmmwanzia@gmail.com>
2024-10-17 12:58:26 +0300

    Update searchWidget.js
    another attempt

--- src/components/searchWidget.js
+++ src/components/searchWidget.js
@@ -3,15 +3,6 @@ import Script from 'next/script'
 import Head from 'next/head'
 import { Box } from '@chakra-ui/react'
 
-const scripts = [
-  "https://reservations.reserveport.com/checkavailability/static/js/manifest.js",
-  "https://reservations.reserveport.com/checkavailability/static/js/vendor.js",
-  "https://reservations.reserveport.com/checkavailability/static/js/app.js",
-  
-  "https://www.reserveport.com/media/api5/jquery.min.js",
-  "https://www.reserveport.com/media/api5/popper.min.js",
-  "https://www.reserveport.com/media/api5/bootstrap.min.js"
-];
 
 const stylesheets = [
   "https://reservations.reserveport.com/checkavailability/static/css/app.css",
@@ -22,24 +13,6 @@ const stylesheets = [
 export default function Home() {
   const [scriptsLoaded, setScriptsLoaded] = useState(false);
   useEffect(() => {
-    // Function to dynamically load external scripts
-    const loadScripts = () => {
-      scripts.forEach(src => {
-        if (!document.querySelector(`script[src="${src}"]`)) {
-          const script = document.createElement("script");
-          script.src = src;
-          script.async = true;
-          script.onload = () => {
-            loadedCount += 1;
-            if (loadedCount === scripts.length) {
-              setScriptsLoaded(true); // All scripts are loaded
-            }
-          };
-          document.body.appendChild(script);
-        }
-      });
-    };
-
     // Function to dynamically load external stylesheets
     const loadStylesheets = () => {
       stylesheets.forEach(href => {
@@ -57,23 +30,54 @@ export default function Home() {
     loadStylesheets();   // Load the external styles when component mounts
   }, []);  // Empty dependency array ensures this runs once on component mount
 
-  return (
-    <>
-    <Head>
-      <Script src="https://reservations.reserveport.com/checkavailability/static/js/manifest.js" strategy="beforeInteractive" />
-      <Script src="https://reservations.reserveport.com/checkavailability/static/js/vendor.js" strategy="beforeInteractive" />
-      <Script src="https://reservations.reserveport.com/checkavailability/static/js/app.js" strategy="beforeInteractive" />
-
-      <Script src="https://www.reserveport.com/media/api5/jquery.min.js" strategy="beforeInteractive" />
-      <Script src="https://www.reserveport.com/media/api5/popper.min.js" strategy="beforeInteractive" />
-      <Script src="https://www.reserveport.com/media/api5/bootstrap.min.js" strategy="beforeInteractive" />
-    </Head>
-    {/* Render the <search-availability> component only when scripts are loaded */}
-    {scriptsLoaded ? (
-        <search-availability id="1687" clientemail="true"></search-availability>
-      ) : (
-        <Box>Loading widget...</Box>
-      )}
-    </>
-  );
-}
+    const handleScriptLoad = () => {
+      // Set scriptsLoaded to true once all necessary scripts are loaded
+      setScriptsLoaded(true);
+    };
+  
+    return (
+      <>
+        <Head>
+          {/* External scripts loaded with Next.js Script component, strategy set to 'beforeInteractive' */}
+          <Script 
+            src="https://reservations.reserveport.com/checkavailability/static/js/manifest.js" 
+            strategy="beforeInteractive" 
+            onLoad={handleScriptLoad} 
+          />
+          <Script 
+            src="https://reservations.reserveport.com/checkavailability/static/js/vendor.js" 
+            strategy="beforeInteractive" 
+            onLoad={handleScriptLoad} 
+          />
+          <Script 
+            src="https://reservations.reserveport.com/checkavailability/static/js/app.js" 
+            strategy="beforeInteractive" 
+            onLoad={handleScriptLoad} 
+          />
+  
+          <Script 
+            src="https://www.reserveport.com/media/api5/jquery.min.js" 
+            strategy="beforeInteractive" 
+            onLoad={handleScriptLoad} 
+          />
+          <Script 
+            src="https://www.reserveport.com/media/api5/popper.min.js" 
+            strategy="beforeInteractive" 
+            onLoad={handleScriptLoad} 
+          />
+          <Script 
+            src="https://www.reserveport.com/media/api5/bootstrap.min.js" 
+            strategy="beforeInteractive" 
+            onLoad={handleScriptLoad} 
+          />
+        </Head>
+  
+        {/* Conditionally render the widget once the scripts are loaded */}
+        {scriptsLoaded ? (
+          <search-availability id="1687" clientemail="true"></search-availability>
+        ) : (
+          <Box>Loading widget...</Box>
+        )}
+      </>
+    );
+  }
