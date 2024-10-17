f3df8ac976dcd70eb45436a6bcc0e9767f2e8240
MwanziaBri <brianmmwanzia@gmail.com>
2024-10-17 12:29:17 +0300

    Update searchWidget.js
    Restored jquery, popper and bootstrap on searchWidget

--- src/components/searchWidget.js
+++ src/components/searchWidget.js
@@ -51,6 +51,13 @@ export default function Home() {
   }, []);  // Empty dependency array ensures this runs once on component mount
 
   return (
+    <>
+    <Head>
+      <Script src="https://www.reserveport.com/media/api5/jquery.min.js" strategy="beforeInteractive" />
+      <Script src="https://www.reserveport.com/media/api5/popper.min.js" strategy="beforeInteractive" />
+      <Script src="https://www.reserveport.com/media/api5/bootstrap.min.js" strategy="beforeInteractive" />
+    </Head>
     <search-availability id="1687" clientemail="true"></search-availability>
+    </>
   );
 }
